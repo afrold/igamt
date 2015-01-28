47d5f1628b287fd630b5886cc5f6ab11b1c13343
Woo <jungyubw@gmail.com>
2015-01-28 16:29:26 -0500

    delete some codes

--- igamt-lite-domain/src/main/java/gov/nist/healthcare/tools/hl7/v2/igamt/lite/service/xml/ProfileSerialization.java
+++
@@ -1,9 +0,0 @@
-package gov.nist.healthcare.tools.hl7.v2.igamt.lite.service.xml;
-
-import gov.nist.healthcare.tools.hl7.v2.igamt.lite.domain.Profile;
-
-public interface ProfileSerialization {
-	Profile deserializeXMLToProfile(String xmlContents);
-	
-	String serializeProfileToXML(Profile profile);
-}
