db7b3032f5f1a47693a66197351af59c478606ee
Woo <jungyubw@gmail.com>
2015-03-03 16:24:12 -0500

    Table Controller

---
+++ igamt-lite-service/src/main/java/gov/nist/healthcare/tools/hl7/v2/igamt/lite/service/repo/TableService.java
@@ -0,0 +1,44 @@
+/**
+ * This software was developed at the National Institute of Standards and Technology by employees
+ * of the Federal Government in the course of their official duties. Pursuant to title 17 Section 105 of the
+ * United States Code this software is not subject to copyright protection and is in the public domain.
+ * This is an experimental system. NIST assumes no responsibility whatsoever for its use by other parties,
+ * and makes no guarantees, expressed or implied, about its quality, reliability, or any other characteristic.
+ * We would appreciate acknowledgement if the software is used. This software can be redistributed and/or
+ * modified freely provided that any derivative works bear some notice that they are derived from it, and any
+ * modified versions bear some notice that they have been modified.
+ */
+
+/**
+ * 
+ * @author Jungyub Woo
+ * 
+ */
+
+package gov.nist.healthcare.tools.hl7.v2.igamt.lite.service.repo;
+
+import gov.nist.healthcare.tools.hl7.v2.igamt.lite.domain.tables.Table;
+import gov.nist.healthcare.tools.hl7.v2.igamt.lite.repo.TableRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class TableService {
+	@Autowired
+	private TableRepository tableRepository;
+
+	public Table save(Table c) {
+		return tableRepository.save(c);
+	}
+
+	public void delete(Long id) {
+		tableRepository.delete(id);
+	}
+
+	public Table findOne(Long id) {
+		return tableRepository.findOne(id);
+	}
+
+}
