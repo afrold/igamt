d79156d5067d9ff61b60f8e6bf77661d94b066ef
Affo, Harold <affoharold@yahoo.fr>
2015-05-27 16:55:00 -0400

    add account db sql

---
+++ igamt-lite-controller/src/main/resources/sql/igl.sql
@@ -0,0 +1,174 @@
+CREATE DATABASE  IF NOT EXISTS `igl` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `igl`;
+-- MySQL dump 10.13  Distrib 5.6.13, for osx10.6 (i386)
+--
+-- Host: 127.0.0.1    Database: igl
+-- ------------------------------------------------------
+-- Server version	5.6.16
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Account`
+--
+
+DROP TABLE IF EXISTS `Account`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Account` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `accountType` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `employer` varchar(100) DEFAULT NULL,
+  `entityDisabled` bit(1) NOT NULL,
+  `fullName` varchar(100) DEFAULT NULL,
+  `juridiction` varchar(100) DEFAULT NULL,
+  `pending` bit(1) NOT NULL,
+  `phone` varchar(100) DEFAULT NULL,
+  `signedConfidentialityAgreement` bit(1) DEFAULT NULL,
+  `title` varchar(100) DEFAULT NULL,
+  `username` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_48j2m0d2pfyl4aehqu34n5a8j` (`fullName`),
+  UNIQUE KEY `UK_de34gsw4qt8auhffbna969ahp` (`username`),
+  UNIQUE KEY `email_UNIQUE` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=45 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Account`
+--
+
+LOCK TABLES `Account` WRITE;
+/*!40000 ALTER TABLE `Account` DISABLE KEYS */;
+INSERT INTO `Account` VALUES (44,'author','haffo@nist.gov','NIST','\0','Harold Affo','NIST','\0','2408988391','','Software Engineer','haffo');
+/*!40000 ALTER TABLE `Account` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `AccountPasswordReset`
+--
+
+DROP TABLE IF EXISTS `AccountPasswordReset`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `AccountPasswordReset` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `currentToken` varchar(255) DEFAULT NULL,
+  `numberOfReset` bigint(20) DEFAULT NULL,
+  `timestamp` datetime DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_i6oajo98rebbp8et98ixj9xvc` (`currentToken`),
+  UNIQUE KEY `UK_q0hkmmtuofykolk4mamqnx393` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `AccountPasswordReset`
+--
+
+LOCK TABLES `AccountPasswordReset` WRITE;
+/*!40000 ALTER TABLE `AccountPasswordReset` DISABLE KEYS */;
+INSERT INTO `AccountPasswordReset` VALUES (1,'aGFmZm8yMDE1NDc1MjI1',6,'2015-05-05 11:15:26','haffo');
+/*!40000 ALTER TABLE `AccountPasswordReset` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Issue`
+--
+
+DROP TABLE IF EXISTS `Issue`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Issue` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `category` varchar(50) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `resolutionComments` varchar(255) DEFAULT NULL,
+  `resolutionTimeStamp` datetime DEFAULT NULL,
+  `resolved` bit(1) NOT NULL,
+  `senderAgent` varchar(255) DEFAULT NULL,
+  `timeStamp` datetime DEFAULT NULL,
+  `title` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Issue`
+--
+
+LOCK TABLES `Issue` WRITE;
+/*!40000 ALTER TABLE `Issue` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Issue` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `authorities`
+--
+
+DROP TABLE IF EXISTS `authorities`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `authorities` (
+  `username` varchar(50) NOT NULL,
+  `authority` varchar(50) NOT NULL,
+  UNIQUE KEY `ix_auth_username` (`username`,`authority`),
+  KEY `fk_authorities_users_idx` (`username`),
+  CONSTRAINT `fk_authorities_users` FOREIGN KEY (`username`) REFERENCES `users` (`username`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `authorities`
+--
+
+LOCK TABLES `authorities` WRITE;
+/*!40000 ALTER TABLE `authorities` DISABLE KEYS */;
+INSERT INTO `authorities` VALUES ('haffo','author'),('haffo','user');
+/*!40000 ALTER TABLE `authorities` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `username` varchar(50) NOT NULL,
+  `password` varchar(256) NOT NULL,
+  `enabled` tinyint(1) NOT NULL,
+  `accountNonExpired` tinyint(1) NOT NULL,
+  `accountNonLocked` tinyint(1) NOT NULL,
+  `credentialsNonExpired` tinyint(1) NOT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-05-27 16:32:46
