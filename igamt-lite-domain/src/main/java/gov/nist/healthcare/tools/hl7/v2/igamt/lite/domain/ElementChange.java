ee0c1a957e7846104e64650cf7eab74b0e7465da
oliviermr <olivier.m.mr@gmail.com>
2015-05-28 15:40:38 -0400

    Support files for the delta

--- igamt-lite-domain/src/main/java/gov/nist/healthcare/tools/hl7/v2/igamt/lite/domain/ElementChange.java
+++ igamt-lite-domain/src/main/java/gov/nist/healthcare/tools/hl7/v2/igamt/lite/domain/ElementChange.java
@@ -1,7 +1,10 @@
 package gov.nist.healthcare.tools.hl7.v2.igamt.lite.domain;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+
+import com.fasterxml.jackson.core.JsonGenerator;
 
 
 public class ElementChange {
@@ -10,35 +13,60 @@ public class ElementChange {
 	 * Id of the element that has modifications 
 	 */
 	private String id;
-	
+
 	/*
 	 * Dictionary where :
 	 * 		key is the name of the getter of the field that has changed 
 	 * 		value is a dictionary with keys "old" and "new" for the new and old values
 	 */
 	private Map<String, Map<String, String>> change; 
-	
-	/*
-	 * Record the type of data corresponding to the id (Message, Datatype, Segment...
-	 */
-	private String type;
 
-	public ElementChange(String id, String type) {
+	/*
+	 * Record the parent of data corresponding to the id (Message, Datatype, Segment...
+	 */
+	private String parent;
+
+	/*
+	 * Record the parent of change : add, del, edit
+	 */
+	private String changeType;
+
+	public ElementChange(String id, String parent) {
 		super();
 		this.id = id;
-		this.type = type;
+		this.parent = parent;
 		this.change = new HashMap<String, Map<String,String>>();
 	}
-	
-	public void recordChange(String field, String oldValue, String newValue){
+
+	public void recordChange(String field, String baseValue, String newValue){
 		Map<String,String> values = new HashMap<String,String>();
-		values.put("old", oldValue);
-		values.put("new", newValue);
+		values.put("basevalue", baseValue);
+		values.put("newvalue", newValue);
 		this.change.put(field, values);
 	}
-	
+
 	public Integer countChanges(){
 		return this.change.size();
+	}
+
+	public void addToJson(JsonGenerator generator) throws IOException{
+		generator.writeStartObject();
+		generator.writeFieldName("id");
+		generator.writeString(this.getId());
+
+		generator.writeArrayFieldStart("changes");
+
+		for (String field: this.getChange().keySet()){
+			generator.writeStartObject();
+			generator.writeFieldName(field);
+			generator.writeStartObject();
+			generator.writeStringField("baseValue", this.getChange().get(field).get("basevalue"));
+			generator.writeStringField("newValue", this.getChange().get(field).get("newvalue"));
+			generator.writeEndObject();
+			generator.writeEndObject();
+		}
+		generator.writeEndArray();
+		generator.writeEndObject();
 	}
 
 	public String getId() {
@@ -49,18 +77,51 @@ public class ElementChange {
 		this.id = id;
 	}
 
-	public String getType() {
-		return type;
+	public String getParent() {
+		return parent;
 	}
 
-	public void setType(String type) {
-		this.type = type;
+	public void setParent(String type) {
+		this.parent = type;
+	}
+
+	public Map<String, Map<String, String>> getChange() {
+		return change;
+	}
+
+	public void setChange(Map<String, Map<String, String>> change) {
+		this.change = change;
+	}
+
+	public String getChangeType() {
+		return changeType;
+	}
+
+	public void setChangeType(String changeType) {
+		this.changeType = changeType;
 	}
 
 	@Override
 	public String toString() {
-		return "ElementChange [id=" + id + ", type="
-				+ type + ", change=" + change +  "]";
+		StringBuilder sb = new StringBuilder();
+		sb.append("ElementChange [id=");
+		sb.append(id);
+		sb.append(", parent=");
+		sb.append(parent);
+		sb.append(", changeType=");
+		sb.append(changeType);
+		sb.append(", change=");
+		for (String field: change.keySet()){
+			sb.append("[");
+			sb.append(field);
+			sb.append(" => base value: ");
+			sb.append(change.get(field).get("basevalue"));
+			sb.append(", new value: ");
+			sb.append(change.get(field).get("newvalue"));
+			sb.append("]");
+		}
+
+		return sb.toString();
 	}
 
 
