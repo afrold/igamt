4bb9f18b6eeecbe989f71ee089e361f1437717d7
gcr-nist <geoffry.roberts@nist.gov>
2015-10-15 08:08:47 -0400

    Little fix to unit test.

---
+++ igamt-lite-client/test/spec/services/hl7-version.js
@@ -0,0 +1,28 @@
+'use strict';
+
+describe("hl7Version service", function () {
+	
+	var HL7VersionSvc;
+	var httpBackend;
+	
+	beforeEach(function() {
+		console.log("0 here==>");
+		module('igl');
+		inject(function (_HL7VersionSvc_, $httpBackend) {{{{{}}}}
+			HL7VersionSvc = _HL7VersionSvc_;
+			console.log("1 here==>");
+	  		httpBackend = $httpBackend;
+		  });
+		console.log(HL7VersionSvc);
+		console.log(httpBackend);
+	});
+	console.log("2 here==>");
+	  
+	it("should get and set", function () {
+		console.log("3 here==>");
+		HL7VersionSvc = "2.5.1";
+		expect("2.5.1").toBe("2.5.0");
+		console.log("4 here==>");
+	});
+	console.log("5 here==>");
+});
