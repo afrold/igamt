60cfe301be47d071c41158fe0fb3d709b833b23d
gcr-nist <geoffry.roberts@nist.gov>
2015-10-22 08:57:27 -0400

    Message context menu 'Clone' option works.

--- igamt-lite-client/app/scripts/controllers/profiles.js
+++ igamt-lite-client/app/scripts/controllers/profiles.js
@@ -564,23 +564,26 @@ angular.module('igl')
 		$scope.closedCtxSubMenu = function(node, $index) {
 			var item = ContextMenuSvc.get();
 			switch (item) {
-			case "Add": 
+			case "Add": {
+				
+			}
+			case "Clone": {
 				var newNode = (JSON.parse(JSON.stringify(node)));
 				newNode.id = null;
 				
 				// Nodes must have unique names so we timestamp when we duplicate.
 				if(newNode.type === 'message') {
-					newNode.messageType = newNode.messageType + "-" + profile.metaData.ext +  "-"  + timeStamp();
+					newNode.messageType = newNode.messageType + "-" + $rootScope.profile.metaData.ext +  "-"  + timeStamp();
 				}
-				for (var i in $scope.profile.messages.children) {
-					console.log($scope.profile.messages.children[i].messageType);
+				for (var i in $rootScope.profile.messages.children) {
+					console.log($rootScope.profile.messages.children[i].messageType);
 				}
-				$scope.profile.messages.children.splice(2, 0, newNode);
-				for (var i in $scope.profile.messages.children) {
-					console.log($scope.profile.messages.children[i].messageType);
+				$rootScope.profile.messages.children.splice(2, 0, newNode);
+				for (var i in $rootScope.profile.messages.children) {
+					console.log($rootScope.profile.messages.children[i].messageType);
 				}
 				break;
-			case "Clone": 
+			}
 			case "Delete": 
 				// not to be implemented at this time.
 				// var nodeInQuestion = $scope.node.messages.children.splice(index, 1);
@@ -590,6 +593,35 @@ angular.module('igl')
 			}
 		};
 
+		function timeStamp() {
+			// Create a date object with the current time
+			  var now = new Date();
+
+			// Create an array with the current month, day and time
+			  var date = [ now.getMonth() + 1, now.getDate(), now.getFullYear() ];
+
+			// Create an array with the current hour, minute and second
+			  var time = [ now.getHours(), now.getMinutes(), now.getSeconds() ];
+
+			// Determine AM or PM suffix based on the hour
+			  var suffix = ( time[0] < 12 ) ? "AM" : "PM";
+
+			// Convert hour from military time
+			  time[0] = ( time[0] < 12 ) ? time[0] : time[0] - 12;
+
+			// If hour is 0, set it to 12
+			  time[0] = time[0] || 12;
+
+			// If seconds and minutes are less than 10, add a zero
+			  for ( var i = 1; i < 3; i++ ) {
+			    if ( time[i] < 10 ) {
+			      time[i] = "0" + time[i];
+			    }
+			  }
+
+			// Return the formatted string
+			  return date.join("/") + " " + time.join(":") + " " + suffix;
+		};
 });
 
 angular.module('igl').controller('ContextMenuCtrl', function ($scope, $rootScope, ContextMenuSvc) {
@@ -754,37 +786,6 @@ angular.module('igl').controller('ConfirmProfileOpenCtrl', function ($scope, $mo
     $scope.cancel = function () {
         $modalInstance.dismiss('cancel');
     };
-
-	function timeStamp() {
-		// Create a date object with the current time
-		  var now = new Date();
-
-		// Create an array with the current month, day and time
-		  var date = [ now.getMonth() + 1, now.getDate(), now.getFullYear() ];
-
-		// Create an array with the current hour, minute and second
-		  var time = [ now.getHours(), now.getMinutes(), now.getSeconds() ];
-
-		// Determine AM or PM suffix based on the hour
-		  var suffix = ( time[0] < 12 ) ? "AM" : "PM";
-
-		// Convert hour from military time
-		  time[0] = ( time[0] < 12 ) ? time[0] : time[0] - 12;
-
-		// If hour is 0, set it to 12
-		  time[0] = time[0] || 12;
-
-		// If seconds and minutes are less than 10, add a zero
-		  for ( var i = 1; i < 3; i++ ) {
-		    if ( time[i] < 10 ) {
-		      time[i] = "0" + time[i];
-		    }
-		  }
-
-		// Return the formatted string
-		  return date.join("/") + " " + time.join(":") + " " + suffix;
-	};
-
 });
 
 
