5cb249c4b162d48aa692e98a2733f51321fd95b5
oliviermr <olivier.m.mr@gmail.com>
2015-11-04 11:22:13 -0500

    Added test suite

---
+++ igamt-lite-service/src/test/java/gov/nist/healthcare/tools/hl7/v2/igamt/lite/service/test/unit/AllTests.java
@@ -0,0 +1,13 @@
+package gov.nist.healthcare.tools.hl7.v2.igamt.lite.service.test.unit;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ ProfileChangeServiceTest.class, ProfileCloneTest.class,
+		ProfileExportTest.class, ProfileLoadStandardTest.class,
+		ProfileVerificationTest.class, SerializationTest.class })
+public class AllTests {
+
+}
