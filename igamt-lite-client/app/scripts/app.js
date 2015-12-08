d7ee4c01d5df7f8d4d10a1f618be1389ca991f9f
jungyubw <jungyub.woo@nist.gov>
2015-12-08 10:31:13 -0500

    Fix login error for server

--- igamt-lite-client/app/scripts/app.js
+++ igamt-lite-client/app/scripts/app.js
@@ -130,8 +130,8 @@ app.config(function ($routeProvider, RestangularProvider, $httpProvider, Keepali
 //            	console.log(config.url);
 //                return "http://localhost:8080/igamt"+ value;
                 if(config.url.startsWith("api")){
-                   config.url = "http://localhost:8080/igamt/"+  config.url;
-                   console.log("config.url=" + config.url);
+//                   config.url = "http://localhost:8080/igamt/"+  config.url;
+//                   console.log("config.url=" + config.url);
                 }
                 return config || $q.when(config);
             }
