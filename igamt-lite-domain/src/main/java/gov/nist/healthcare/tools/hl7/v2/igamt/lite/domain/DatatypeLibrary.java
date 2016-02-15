d648178a69334c32eaf4c5cf37097aeb213a2e26
gcr-nist <geoffry.roberts@nist.gov>
2016-02-15 12:18:04 -0500

    DnD works

---
+++ igamt-lite-domain/src/main/java/gov/nist/healthcare/tools/hl7/v2/igamt/lite/domain/DatatypeLibrary.java
@@ -0,0 +1,238 @@
+package gov.nist.healthcare.tools.hl7.v2.igamt.lite.domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import gov.nist.healthcare.tools.hl7.v2.igamt.lite.domain.constraints.ConformanceStatement;
+import gov.nist.healthcare.tools.hl7.v2.igamt.lite.domain.constraints.Predicate;
+
+@Document(collection = "datatype-library")
+public class DatatypeLibrary extends TextbasedSectionModel implements java.io.Serializable, Cloneable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String id;
+
+	private Long accountId;
+	
+	public enum SCOPE {HL7STANDARD, MASTER, USER};
+ 
+	public DatatypeLibrary() {
+		super();
+		this.id = ObjectId.get().toString();
+	}
+	
+	private SCOPE scope;
+
+	public SCOPE getScope() {
+		return scope;
+	}
+
+	public void setScope(SCOPE scope) {
+		this.scope = scope;
+	}
+
+	@DBRef
+	private Set<Datatype> children = new HashSet<Datatype>();
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public Long getAccountId() {
+		return accountId;
+	}
+
+	public void setAccountId(Long accountId) {
+		this.accountId = accountId;
+	}
+
+	public Set<Datatype> getChildren() {
+		return children;
+	}
+
+	public void setChildren(Set<Datatype> children) {
+		this.children = children;
+	}
+
+	public void addDatatype(Datatype d) {
+		children.add(d);
+	}
+
+	public Datatype save(Datatype d) {
+		if (!this.children.contains(d)) {
+			children.add(d);
+		}
+		return d;
+	}
+
+	public void delete(String id) {
+		Datatype d = findOne(id);
+		if (d != null)
+			this.children.remove(d);
+	}
+
+	public Datatype findOne(String id) {
+		if (this.children != null) {
+			for (Datatype m : this.children) {
+				if (m.getId().equals(id)) {
+					return m;
+				}
+			}
+		}
+
+		return null;
+	}
+
+	public Datatype findOneByNameAndByLabel(String name, String label) {
+		if (this.children != null) {
+			for (Datatype dt : this.children) {
+				if (dt.getName().equals(name) 
+						&& dt.getLabel().equals(label)) {
+					return dt;
+				}
+			}
+		}
+		return null;
+	}
+	
+	public Component findOneComponent(String id) {
+		if (this.children != null)
+			for (Datatype m : this.children) {
+				Component c = findOneComponent(id, m);
+				if (c != null) {
+					return c;
+				}
+			}
+
+		return null;
+	}
+
+	public Component findOneComponent(String id, Datatype datatype) {
+		if (datatype.getComponents() != null) {
+			for (Component c : datatype.getComponents()) {
+				if (c.getId().equals(id)) {
+					return c;
+				} else {
+					Component r = findOneComponent(id,
+							this.findOne(c.getDatatype()));
+					if (r != null) {
+						return r;
+					}
+				}
+			}
+		}
+		return null;
+	}
+
+	public Datatype findOneDatatypeByLabel(String label) {
+		if (this.children != null)
+			for (Datatype d : this.children) {
+				if (d.getLabel().equals(label)) {
+					return d;
+				}
+			}
+		return null;
+	}
+	
+	public Datatype findOneDatatypeByBase(String baseName){
+		if (this.children != null)
+			for (Datatype d : this.children){
+				if(d.getName().equals(baseName)) {
+					return d;
+				}
+			}
+		return null;
+	}
+
+	public Predicate findOnePredicate(String predicateId) {
+		for (Datatype datatype : this.getChildren()) {
+			Predicate predicate = datatype.findOnePredicate(predicateId);
+			if (predicate != null) {
+				return predicate;
+			}
+		}
+		return null;
+	}
+
+	public ConformanceStatement findOneConformanceStatement(
+			String conformanceStatementId) {
+		for (Datatype datatype : this.getChildren()) {
+			ConformanceStatement conf = datatype
+					.findOneConformanceStatement(conformanceStatementId);
+			if (conf != null) {
+				return conf;
+			}
+		}
+		return null;
+	}
+
+	public boolean deletePredicate(String predicateId) {
+		for (Datatype datatype : this.getChildren()) {
+			if (datatype.deletePredicate(predicateId)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public boolean deleteConformanceStatement(String confStatementId) {
+		for (Datatype datatype : this.getChildren()) {
+			if (datatype.deleteConformanceStatement(confStatementId)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public DatatypeLibrary clone(HashMap<String, Datatype> dtRecords,
+			HashMap<String, Table> tableRecords)
+			throws CloneNotSupportedException {
+		DatatypeLibrary clonedDatatypes = new DatatypeLibrary();
+		clonedDatatypes.setChildren(new HashSet<Datatype>());
+		for (Datatype dt : this.children) {
+			if (dtRecords.containsKey(dt.getId())) {
+				clonedDatatypes.addDatatype(dtRecords.get(dt.getId()));
+			} else {
+				Datatype clone = dt.clone();
+				clone.setId(dt.getId());
+				dtRecords.put(dt.getId(), clone);
+				clonedDatatypes.addDatatype(clone);
+			}
+		}
+
+		return clonedDatatypes;
+	}
+	
+	public void merge(DatatypeLibrary dts){
+		for (Datatype dt : dts.getChildren()){
+			if (this.findOneByNameAndByLabel(dt.getName(), dt.getLabel()) == null){
+				this.addDatatype(dt);
+			} else {
+				dt.setId(this.findOneByNameAndByLabel(dt.getName(), dt.getLabel()).getId()); //FIXME Probably useless...
+			}
+		}
+		
+	}
+	
+	public void setPositionsOrder(){
+		List<Datatype> sortedList = new ArrayList<Datatype>(this.getChildren());
+		Collections.sort(sortedList);
+		for (Datatype elt: sortedList) {
+			elt.setSectionPosition(sortedList.indexOf(elt));
+		}
+	}
+
+}
