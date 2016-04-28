96454d3b78476f0eb88af2ec0a088a9c8d91f0c1
gcr-nist <geoffry.roberts@nist.gov>
2016-04-28 13:20:36 -0400

    Contains datatype master library

--- igamt-lite-client/app/scripts/controllers/DatatypeLibraryCtl.js
+++ igamt-lite-client/app/scripts/controllers/DatatypeLibraryCtl.js
@@ -7,7 +7,7 @@ angular.module('igl').controller('MasterDatatypeLibraryCtl',
 			$scope.datatypeStruct = false;
 			$scope.loadingSelection = true;
 			$scope.publishSelections = [];
-			$scope.datatypeDisplay	= [];		
+			$scope.datatypeDisplay	= [];
 	        $scope.viewSettings = ViewSettings;
 
 	        $scope.tableWidth = null;
@@ -15,8 +15,11 @@ angular.module('igl').controller('MasterDatatypeLibraryCtl',
 			$scope.initDatatypeLibrary = function() {
 				$scope.start = false;
 				getDataTypeLibrary("MASTER");
+/*         $http.get('api/datatypes/12345').then(function(response){
+          console.log(JSONStingify(response));
+        }); */
 			};
-			
+
 			function getDataTypeLibrary(scope) {
 				DatatypeLibrarySvc.getDataTypeLibrary(scope).then(function(data) {
 						$scope.datatypeStruct =  data;
@@ -28,13 +31,13 @@ angular.module('igl').controller('MasterDatatypeLibraryCtl',
 					console.log(error);
 				});
 			};
-			
+
 			$scope.getDisplayLabel = function(dt) {
 				if(dt) {
 					return (dt.ext) ? dt.label + "_" + dt.ext : dt.label;
 				}
-			} 
-			
+			}
+
 			$scope.trackSelections = function(bool, selection) {
 				if (bool) {
 					selection.status = "PUBLISHED";
@@ -42,14 +45,14 @@ angular.module('igl').controller('MasterDatatypeLibraryCtl',
 					selection.status = "UNPUBLISHED";
 				}
 			};
-			
+
 			$scope.save = function() {
 				angular.forEach($scope.datatypeStruct.children, function(dt) {
 					delete dt.new;
 				});
 				DatatypeLibrarySvc.save($scope.datatypeStruct);
 			};
-			
+
 			$scope.edit = function(datatype) {
 				$scope.loadingSelection = true;
 				$timeout(
@@ -60,7 +63,7 @@ angular.module('igl').controller('MasterDatatypeLibraryCtl',
 		                		$scope.datatypesParams.refresh();
 				}, 100);
 			};
-			
+
 	        $scope.datatypesParams = new ngTreetableParams({
 	            getNodes: function (parent) {
 	                return DatatypeService.getNodes(parent);
@@ -69,7 +72,7 @@ angular.module('igl').controller('MasterDatatypeLibraryCtl',
 	                return DatatypeService.getTemplate(node);
 	            }
 	        });
-		
+
 //	        $scope.selectDatatype = function (datatype) {
 //	            $scope.subview = "EditDatatypes.html";
 //	            if (datatype && datatype != null) {
@@ -89,7 +92,7 @@ angular.module('igl').controller('MasterDatatypeLibraryCtl',
 //	                    }, 100);
 //	            }
 //	        };
-	        
+
 //	        $scope.getTableWidth = function () {
 //	            if ($scope.tableWidth === null || $scope.tableWidth == 0) {
 //	                $scope.tableWidth = $("#nodeDetailsPanel").width();
@@ -105,7 +108,7 @@ angular.module('igl').controller('MasterDatatypeLibraryCtl',
 //	            }
 //	            return "";
 //	        };
-			
+
 			$scope.openStandardDataypes = function() {
 				DatatypeLibrarySvc.getDataTypeLibrary("HL7STANDARD").then(function(data) {
 					var datatypeStruct = data;
@@ -130,7 +133,7 @@ angular.module('igl').controller('MasterDatatypeLibraryCtl',
 				console.log(error);
 			});
 		};
-		
+
 		function decoratedSelections(datatypes) {
 			angular.forEach(datatypes, function(child) {
 				child.ext = Math.floor(Math.random() * 100);
@@ -143,18 +146,18 @@ angular.module('igl').controller('MasterDatatypeLibraryCtl',
 
 angular.module('igl').controller('StandardDatatypeLibraryInstanceDlgCtl',
 		function($scope, $modalInstance, datatypeStruct, DatatypeLibrarySvc) {
-			
+
 			$scope.okDisabled = true;
 
-			$scope.datatypeStruct = datatypeStruct;	
+			$scope.datatypeStruct = datatypeStruct;
 			$scope.standardSelections = [];
-			
+
 			$scope.getDisplayLabel = function(dt) {
 				if (dt) {
 					return dt.label;
 				}
-			} 
-			
+			}
+
 			$scope.trackSelections = function(bool, event) {
 				if (bool) {
 					$scope.standardSelections.push(event);
@@ -175,5 +178,5 @@ angular.module('igl').controller('StandardDatatypeLibraryInstanceDlgCtl',
 			$scope.cancel = function() {
 				$modalInstance.dismiss('cancel');
 			};
-			
+
 		});
