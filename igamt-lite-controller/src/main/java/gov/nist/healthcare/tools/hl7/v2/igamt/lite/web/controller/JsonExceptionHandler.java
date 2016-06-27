50350de24bb99c736f9abc49a74bc6eced051e7c
Affo, Harold <affoharold@yahoo.fr>
2016-06-27 13:43:31 -0400

    adding operation checks

--- igamt-lite-controller/src/main/java/gov/nist/healthcare/tools/hl7/v2/igamt/lite/web/controller/JsonExceptionHandler.java
+++ igamt-lite-controller/src/main/java/gov/nist/healthcare/tools/hl7/v2/igamt/lite/web/controller/JsonExceptionHandler.java
@@ -1,12 +1,13 @@
 /**
- * This software was developed at the National Institute of Standards and Technology by employees
- * of the Federal Government in the course of their official duties. Pursuant to title 17 Section 105 of the
- * United States Code this software is not subject to copyright protection and is in the public domain.
- * This is an experimental system. NIST assumes no responsibility whatsoever for its use by other parties,
- * and makes no guarantees, expressed or implied, about its quality, reliability, or any other characteristic.
- * We would appreciate acknowledgment if the software is used. This software can be redistributed and/or
- * modified freely provided that any derivative works bear some notice that they are derived from it, and any
- * modified versions bear some notice that they have been modified.
+ * This software was developed at the National Institute of Standards and Technology by employees of
+ * the Federal Government in the course of their official duties. Pursuant to title 17 Section 105
+ * of the United States Code this software is not subject to copyright protection and is in the
+ * public domain. This is an experimental system. NIST assumes no responsibility whatsoever for its
+ * use by other parties, and makes no guarantees, expressed or implied, about its quality,
+ * reliability, or any other characteristic. We would appreciate acknowledgment if the software is
+ * used. This software can be redistributed and/or modified freely provided that any derivative
+ * works bear some notice that they are derived from it, and any modified versions bear some notice
+ * that they have been modified.
  * */
 package gov.nist.healthcare.tools.hl7.v2.igamt.lite.web.controller;
 
@@ -17,6 +18,7 @@ import gov.nist.healthcare.tools.hl7.v2.igamt.lite.service.IGDocumentException;
 import gov.nist.healthcare.tools.hl7.v2.igamt.lite.service.IGDocumentListException;
 import gov.nist.healthcare.tools.hl7.v2.igamt.lite.service.IGDocumentNotFoundException;
 import gov.nist.healthcare.tools.hl7.v2.igamt.lite.service.IGDocumentSaveException;
+import gov.nist.healthcare.tools.hl7.v2.igamt.lite.web.exception.DataNotFoundException;
 import gov.nist.healthcare.tools.hl7.v2.igamt.lite.web.exception.DatatypeSaveException;
 import gov.nist.healthcare.tools.hl7.v2.igamt.lite.web.exception.OperationNotAllowException;
 import gov.nist.healthcare.tools.hl7.v2.igamt.lite.web.exception.SegmentDeleteException;
@@ -39,101 +41,105 @@ import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
- * Called when an exception occurs during request processing. Transforms the
- * exception message into JSON format.
+ * Called when an exception occurs during request processing. Transforms the exception message into
+ * JSON format.
  */
 @Component
 public class JsonExceptionHandler implements HandlerExceptionResolver {
-	private final ObjectMapper mapper = new ObjectMapper();
+  private final ObjectMapper mapper = new ObjectMapper();
 
-	static final Logger logger = LoggerFactory
-			.getLogger(JsonExceptionHandler.class);
+  static final Logger logger = LoggerFactory.getLogger(JsonExceptionHandler.class);
 
-	@Override
-	public ModelAndView resolveException(HttpServletRequest request,
-			HttpServletResponse response, Object handler, Exception ex) {
-		try {
-			if (ex instanceof AccessDeniedException) {
-				logger.error("ERROR: Access Denied", ex);
-				response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-				mapper.writeValue(response.getWriter(), new ResponseMessage(
-						ResponseMessage.Type.danger, "accessDenied"));
-			} else if (ex instanceof UserAccountNotFoundException) {
-				logger.error("ERROR: User account not found", ex);
-				response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-				mapper.writeValue(response.getWriter(), new ResponseMessage(
-						ResponseMessage.Type.danger, "accountNotFound", null));
-			} else if (ex instanceof IGDocumentNotFoundException) {
-				logger.error("ERROR: document not found", ex);
-				response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-				mapper.writeValue(response.getWriter(), new ResponseMessage(
-						ResponseMessage.Type.danger, "IGDocumentNotFound"));
-			} else if (ex instanceof IGDocumentSaveException) {
-				logger.error("ERROR: Access Denied", ex);
-				response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-				mapper.writeValue(response.getWriter(), new ResponseMessage(
-						ResponseMessage.Type.danger, "igDocumentNotSaved"));
-			} else if (ex instanceof IGDocumentDeleteException) {
-				logger.error("ERROR: Access Denied", ex);
-				response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-				mapper.writeValue(response.getWriter(), new ResponseMessage(
-						ResponseMessage.Type.danger, "igDocumentNotDeleted"));
-			} else if (ex instanceof OperationNotAllowException) {
-				logger.error("ERROR: Access Denied", ex);
-				response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-				mapper.writeValue(response.getWriter(), new ResponseMessage(
-						ResponseMessage.Type.danger, "operationNotAllow"));
-			} else if (ex instanceof IGDocumentException) {
-				logger.error("ERROR: Access Denied", ex);
-				response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-				mapper.writeValue(response.getWriter(), new ResponseMessage(
-						ResponseMessage.Type.danger, "igDocumentIssue"));
-			} else if (ex instanceof IGDocumentListException) {
-				logger.error("ERROR: Access Denied", ex);
-				response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-				mapper.writeValue(response.getWriter(), new ResponseMessage(
-						ResponseMessage.Type.danger, "igDocumentListFailed"));
-			} else if (ex instanceof UploadImageFileException) {
-				logger.error("ERROR: Failed to upload the image file", ex);
-				response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-				mapper.writeValue(response.getWriter(), new ResponseMessage(
-						ResponseMessage.Type.danger, ex.getMessage()));
-			} else if (ex instanceof SegmentSaveException) {
-				logger.error("ERROR: Failed to save the segment", ex);
-				response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-				mapper.writeValue(response.getWriter(), new ResponseMessage(
-						ResponseMessage.Type.danger, "segmentSaveFailed"));
-			} else if (ex instanceof TableSaveException) {
-				logger.error("ERROR: Failed to save the table", ex);
-				response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-				mapper.writeValue(response.getWriter(), new ResponseMessage(
-						ResponseMessage.Type.danger, "tableSaveFailed"));
-			} else if (ex instanceof DatatypeSaveException) {
-				logger.error("ERROR: Failed to save the datatype", ex);
-				response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-				mapper.writeValue(response.getWriter(), new ResponseMessage(
-						ResponseMessage.Type.danger, "datatypeSaveFailed"));
-			} else if (ex instanceof ForbiddenOperationException) {
-				logger.error("ERROR: Failed to execute operation", ex);
-				response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-				mapper.writeValue(response.getWriter(), new ResponseMessage(
-						ResponseMessage.Type.danger, "forbiddenOperation"));
-			} else if (ex instanceof SegmentDeleteException) {
-				logger.error("ERROR: Failed to delete a segment", ex);
-				response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-				mapper.writeValue(response.getWriter(), new ResponseMessage(
-						ResponseMessage.Type.danger, ex.getMessage()));
-			} else {
-				logger.error("ERROR: " + ex.getMessage(), ex);
-				response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-				mapper.writeValue(response.getWriter(), new ResponseMessage(
-						ResponseMessage.Type.danger, "internalError"));
-			}
-		} catch (IOException e) {
-			// give up
-			logger.error("ERROR: GAVE UP: " + e.getMessage(), e);
-		}
-		return new ModelAndView();
-	}
+  @Override
+  public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response,
+      Object handler, Exception ex) {
+    try {
+      if (ex instanceof AccessDeniedException) {
+        logger.error("ERROR: Access Denied", ex);
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        mapper.writeValue(response.getWriter(), new ResponseMessage(ResponseMessage.Type.danger,
+            "accessDenied"));
+      } else if (ex instanceof UserAccountNotFoundException) {
+        logger.error("ERROR: User account not found", ex);
+        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        mapper.writeValue(response.getWriter(), new ResponseMessage(ResponseMessage.Type.danger,
+            "accountNotFound", null));
+      } else if (ex instanceof IGDocumentNotFoundException) {
+        logger.error("ERROR: document not found", ex);
+        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        mapper.writeValue(response.getWriter(), new ResponseMessage(ResponseMessage.Type.danger,
+            "IGDocumentNotFound"));
+      } else if (ex instanceof IGDocumentSaveException) {
+        logger.error("ERROR: Access Denied", ex);
+        response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        mapper.writeValue(response.getWriter(), new ResponseMessage(ResponseMessage.Type.danger,
+            "igDocumentNotSaved"));
+      } else if (ex instanceof IGDocumentDeleteException) {
+        logger.error("ERROR: Access Denied", ex);
+        response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        mapper.writeValue(response.getWriter(), new ResponseMessage(ResponseMessage.Type.danger,
+            "igDocumentNotDeleted"));
+      } else if (ex instanceof OperationNotAllowException) {
+        logger.error("ERROR: Access Denied", ex);
+        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        mapper.writeValue(response.getWriter(), new ResponseMessage(ResponseMessage.Type.danger,
+            "operationNotAllow"));
+      } else if (ex instanceof IGDocumentException) {
+        logger.error("ERROR: Access Denied", ex);
+        response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        mapper.writeValue(response.getWriter(), new ResponseMessage(ResponseMessage.Type.danger,
+            "igDocumentIssue"));
+      } else if (ex instanceof IGDocumentListException) {
+        logger.error("ERROR: Access Denied", ex);
+        response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        mapper.writeValue(response.getWriter(), new ResponseMessage(ResponseMessage.Type.danger,
+            "igDocumentListFailed"));
+      } else if (ex instanceof UploadImageFileException) {
+        logger.error("ERROR: Failed to upload the image file", ex);
+        response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        mapper.writeValue(response.getWriter(),
+            new ResponseMessage(ResponseMessage.Type.danger, ex.getMessage()));
+      } else if (ex instanceof SegmentSaveException) {
+        logger.error("ERROR: Failed to save the segment", ex);
+        response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        mapper.writeValue(response.getWriter(), new ResponseMessage(ResponseMessage.Type.danger,
+            "segmentSaveFailed"));
+      } else if (ex instanceof TableSaveException) {
+        logger.error("ERROR: Failed to save the table", ex);
+        response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        mapper.writeValue(response.getWriter(), new ResponseMessage(ResponseMessage.Type.danger,
+            "tableSaveFailed"));
+      } else if (ex instanceof DatatypeSaveException) {
+        logger.error("ERROR: Failed to save the datatype", ex);
+        response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        mapper.writeValue(response.getWriter(), new ResponseMessage(ResponseMessage.Type.danger,
+            "datatypeSaveFailed"));
+      } else if (ex instanceof ForbiddenOperationException) {
+        logger.error("ERROR: Failed to execute operation", ex);
+        response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        mapper.writeValue(response.getWriter(), new ResponseMessage(ResponseMessage.Type.danger,
+            "forbiddenOperation"));
+      } else if (ex instanceof SegmentDeleteException) {
+        logger.error("ERROR: Failed to delete a segment", ex);
+        response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        mapper.writeValue(response.getWriter(),
+            new ResponseMessage(ResponseMessage.Type.danger, ex.getMessage()));
+      } else if (ex instanceof DataNotFoundException) {
+        logger.error("ERROR: Failed to retrieve a data", ex);
+        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        mapper.writeValue(response.getWriter(),
+            new ResponseMessage(ResponseMessage.Type.danger, ex.getMessage()));
+      } else {
+        logger.error("ERROR: " + ex.getMessage(), ex);
+        response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        mapper.writeValue(response.getWriter(), new ResponseMessage(ResponseMessage.Type.danger,
+            "internalError"));
+      }
+    } catch (IOException e) {
+      // give up
+      logger.error("ERROR: GAVE UP: " + e.getMessage(), e);
+    }
+    return new ModelAndView();
+  }
 
 }
--- igamt-lite-controller/src/main/java/gov/nist/healthcare/tools/hl7/v2/igamt/lite/web/controller/TableController.java
+++ igamt-lite-controller/src/main/java/gov/nist/healthcare/tools/hl7/v2/igamt/lite/web/controller/TableController.java
@@ -2,11 +2,13 @@ package gov.nist.healthcare.tools.hl7.v2.igamt.lite.web.controller;
 
 import gov.nist.healthcare.nht.acmgt.repo.AccountRepository;
 import gov.nist.healthcare.nht.acmgt.service.UserService;
+import gov.nist.healthcare.tools.hl7.v2.igamt.lite.domain.Constant.SCOPE;
 import gov.nist.healthcare.tools.hl7.v2.igamt.lite.domain.Table;
 import gov.nist.healthcare.tools.hl7.v2.igamt.lite.service.ForbiddenOperationException;
 import gov.nist.healthcare.tools.hl7.v2.igamt.lite.service.TableLibraryService;
 import gov.nist.healthcare.tools.hl7.v2.igamt.lite.service.TableService;
 import gov.nist.healthcare.tools.hl7.v2.igamt.lite.web.DateUtils;
+import gov.nist.healthcare.tools.hl7.v2.igamt.lite.web.exception.DataNotFoundException;
 import gov.nist.healthcare.tools.hl7.v2.igamt.lite.web.exception.TableSaveException;
 
 import java.util.List;
@@ -24,52 +26,67 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/tables")
 public class TableController extends CommonController {
-	static final Logger logger = LoggerFactory.getLogger(TableController.class);
-	Logger log = LoggerFactory.getLogger(TableController.class);
+  static final Logger logger = LoggerFactory.getLogger(TableController.class);
+  Logger log = LoggerFactory.getLogger(TableController.class);
 
-	@Autowired
-	private TableService tableService;
+  @Autowired
+  private TableService tableService;
 
-	@Autowired
-	private TableLibraryService tableLibraryService;
+  @Autowired
+  private TableLibraryService tableLibraryService;
 
-	@Autowired
-	UserService userService;
+  @Autowired
+  UserService userService;
 
-	@Autowired
-	AccountRepository accountRepository;
+  @Autowired
+  AccountRepository accountRepository;
 
-	@RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = "application/json")
-	public Table getTableById(@PathVariable("id") String id) {
-		log.info("Fetching tableById..." + id);
-		Table result = tableService.findById(id);
-		return result;
-	}
+  @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = "application/json")
+  public Table getTableById(@PathVariable("id") String id) throws DataNotFoundException {
+    log.info("Fetching tableById..." + id);
+    return findById(id);
+  }
 
-	@RequestMapping(value = "/save", method = RequestMethod.POST)
-	public Table save(@RequestBody Table table) throws TableSaveException,
-			ForbiddenOperationException {
-		log.debug("table=" + table);
-		log.debug("table.getId()=" + table.getId());
-		log.info("Saving the " + table.getScope() + " table.");
-		table.setDate(DateUtils.getCurrentTime());
-		Table saved = tableService.save(table);
-		log.debug("saved.getId()=" + saved.getId());
-		log.debug("saved.getScope()=" + saved.getScope());
-		return table;
+  @RequestMapping(value = "/save", method = RequestMethod.POST)
+  public Table save(@RequestBody Table table) throws TableSaveException,
+      ForbiddenOperationException {
+    if (!SCOPE.HL7STANDARD.equals(table.getScope())) {
+      log.debug("table=" + table);
+      log.debug("table.getId()=" + table.getId());
+      log.info("Saving the " + table.getScope() + " table.");
+      table.setDate(DateUtils.getCurrentTime());
+      Table saved = tableService.save(table);
+      log.debug("saved.getId()=" + saved.getId());
+      log.debug("saved.getScope()=" + saved.getScope());
+      return table;
+    } else {
+      throw new ForbiddenOperationException("FORBIDDEN_SAVE_TABLE");
+    }
+  }
 
-	}
-
-	@RequestMapping(value = "/{id}/delete", method = RequestMethod.POST)
-	public boolean delete(@PathVariable("id") String tableId) {
-		log.info("Deleting table " + tableId);
-		tableService.delete(tableId);
-		return true;
-	}
+  @RequestMapping(value = "/{id}/delete", method = RequestMethod.POST)
+  public boolean delete(@PathVariable("id") String tableId) throws ForbiddenOperationException,
+      DataNotFoundException {
+    Table table = findById(tableId);
+    if (!SCOPE.HL7STANDARD.equals(table.getScope())) {
+      log.info("Deleting table " + tableId);
+      tableService.delete(tableId);
+      return true;
+    } else {
+      throw new ForbiddenOperationException("FORBIDDEN_DELETE_TABLE");
+    }
+  }
 
   @RequestMapping(value = "/findAllByIds", method = RequestMethod.POST)
   public List<Table> collect(@RequestBody Set<String> tableIds) {
     return tableService.findAllByIds(tableIds);
+  }
+
+  public Table findById(String id) throws DataNotFoundException {
+    Table result = tableService.findById(id);
+    if (result == null)
+      throw new DataNotFoundException("tableNotFound");
+    return result;
   }
 
 
