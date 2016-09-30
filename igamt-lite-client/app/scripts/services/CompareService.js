851c0afcf8fb510cbedf33516a53c723e1f716b3
Ismail Mellouli <ismail.mellouli@nist.gov>
2016-09-30 11:46:10 -0400

    delta fix

--- igamt-lite-client/app/scripts/services/CompareService.js
+++ igamt-lite-client/app/scripts/services/CompareService.js
@@ -228,6 +228,10 @@ angular.module('igl').factory('CompareService',
 
                         }
                     };
+                    console.log(fields[i].datatype.ext);
+                    if (fields[i].datatype.ext === "") {
+                        fields[i].datatype.ext = null;
+                    }
                     fields[i].datatype.id = "";
 
                 };
@@ -262,8 +266,7 @@ angular.module('igl').factory('CompareService',
                             };
                         }
                         if (childArray.value.type.value === "field" || childArray.value.type.value === "component" || childArray.value.type.value === "datatype") {
-                            console.log("++++++++++++++++++++");
-                            console.log(childArray);
+
                             if (childArray.value.name.changed === "primitive change") {
                                 result.label = {
                                     element1: childArray.value.name.removed,
@@ -280,12 +283,23 @@ angular.module('igl').factory('CompareService',
 
                                 };
                             }
+                            // console.log("-----------------------");
+                            // console.log(childArray);
                             if (childArray.value.ext && childArray.value.ext.changed === "primitive change") {
-                                result.ext = {
-                                    element1: childArray.value.ext.removed,
-                                    element2: childArray.value.name.added
+                                // console.log("++++++++++++++++++++");
+                                // console.log(childArray);
+                                if ((childArray.value.ext.removed === "" && childArray.value.ext.added === null) || (childArray.value.ext.removed === null && childArray.value.ext.added === "")) {
+                                    result.ext = {
+                                        element: ""
+                                    }
+                                } else {
+                                    result.ext = {
+                                        element1: childArray.value.ext.removed,
+                                        element2: childArray.value.ext.added
 
-                                };
+                                    };
+                                }
+
 
                             }
 
