28a9def1f60324d924ee8434565a9eea2b09f047
Lefort, Maxence C <maxence.lefort@nist.gov>
2016-12-07 14:35:45 -0500

    Create class structure for the new serialization service

---
+++ igamt-lite-service/src/main/java/gov/nist/healthcare/tools/hl7/v2/igamt/lite/service/impl/SerializationServiceImpl.java
@@ -0,0 +1,37 @@
+package gov.nist.healthcare.tools.hl7.v2.igamt.lite.service.impl;
+
+import gov.nist.healthcare.tools.hl7.v2.igamt.lite.domain.IGDocument;
+import gov.nist.healthcare.tools.hl7.v2.igamt.lite.domain.SerializableElement;
+import gov.nist.healthcare.tools.hl7.v2.igamt.lite.domain.SerializableStructure;
+import gov.nist.healthcare.tools.hl7.v2.igamt.lite.service.SerializationService;
+import nu.xom.Document;
+
+/**
+ * This software was developed at the National Institute of Standards and Technology by employees of
+ * the Federal Government in the course of their official duties. Pursuant to title 17 Section 105
+ * of the United States Code this software is not subject to copyright protection and is in the
+ * public domain. This is an experimental system. NIST assumes no responsibility whatsoever for its
+ * use by other parties, and makes no guarantees, expressed or implied, about its quality,
+ * reliability, or any other characteristic. We would appreciate acknowledgement if the software is
+ * used. This software can be redistributed and/or modified freely provided that any derivative
+ * works bear some notice that they are derived from it, and any modified versions bear some notice
+ * that they have been modified.
+ * <p>
+ * Created by Maxence Lefort on 12/7/16.
+ */
+public class SerializationServiceImpl implements SerializationService {
+
+    @Override public Document serializeIGDocument(IGDocument igDocument) {
+        return null;
+    }
+
+    @Override public Document serializeDatatypeLibrary(IGDocument igDocument) {
+        return null;
+    }
+
+    @Override public Document serializeElement(SerializableElement element) {
+        SerializableStructure serializableStructure = new SerializableStructure();
+        serializableStructure.addSerializableElement(element);
+        return serializableStructure.serializeStructure();
+    }
+}
