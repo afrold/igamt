f57a5c23ae25a13c76d723cd08739c07be0f9a6a
El Ouakili, Abdelghani (IntlAssoc) <abdelghani.elouakili@nist.net>
2017-01-05 15:15:14 -0500

    Fix adding master library

--- igamt-lite-controller/src/main/java/gov/nist/healthcare/tools/hl7/v2/igamt/lite/web/config/Bootstrap.java
+++ igamt-lite-controller/src/main/java/gov/nist/healthcare/tools/hl7/v2/igamt/lite/web/config/Bootstrap.java
@@ -144,11 +144,11 @@ public class Bootstrap implements InitializingBean {
     	  
 	  
 	  // ===============Data Type Library=====================================
-	 CreateCollectionOfUnchanged(); // group datatype by sets of versions 
-	 setDtsStatus();// sets the status of all the datatypes to published or unpublished 
-	 setTablesStatus(); //  sets the status of all the tables to published or unpublished 
-     Colorate(); // genenerates the datatypes evolution matrix. 
-     setSegmentStatus();
+	// CreateCollectionOfUnchanged(); // group datatype by sets of versions 
+	// setDtsStatus();// sets the status of all the datatypes to published or unpublished 
+	// setTablesStatus(); //  sets the status of all the tables to published or unpublished 
+    // Colorate(); // genenerates the datatypes evolution matrix. 
+   //  setSegmentStatus();
 	  //====================================================================*/
 	//  this.modifyConstraint();
 //	  this.modifyMSH2Constraint();
