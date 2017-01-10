72ba7b89ae596e9d4d480ec2594e52b5b768b6e2
El Ouakili, Abdelghani (IntlAssoc) <abdelghani.elouakili@nist.net>
2017-01-10 17:10:42 -0500

    fix sections

--- igamt-lite-client/app/scripts/controllers/about.js
+++ igamt-lite-client/app/scripts/controllers/about.js
@@ -20,7 +20,7 @@ angular.module('igl')
         },
         {
             "version":'1.0.0-beta-2',
-            "date":'03/07/2016',
+            "date":'01/10/2017',
             updates:[
                 'Display the left side table of contents for HTML export','Add Image (pfg,gif,jpeg,jpg) upload feature',' Add File (word, html,pdf) Upload feature','Export IG Document as Word Document',
                 'Display the list message events by version for the creation of a new IG Document','Handle the message level predicates and conformance statements','Added Issue and About Tabs'
