6d9e75fc4a2222933f812f9015670e1fd55fff7d
Lefort, Maxence C <maxence.lefort@nist.gov>
2017-01-22 02:48:43 -0500

    Do not serialize null value sets

--- igamt-lite-domain/src/main/java/gov/nist/healthcare/tools/hl7/v2/igamt/lite/domain/serialization/SerializableSection.java
+++ igamt-lite-domain/src/main/java/gov/nist/healthcare/tools/hl7/v2/igamt/lite/domain/serialization/SerializableSection.java
@@ -55,7 +55,9 @@ public class SerializableSection extends SerializableElement {
     @Override
     public Element serializeElement() {
         for(SerializableSection serializableSection : serializableSectionList){
-            sectionElement.appendChild(serializableSection.serializeElement());
+            if(serializableSection!=null) {
+                sectionElement.appendChild(serializableSection.serializeElement());
+            }
         }
         return sectionElement;
     }
