1e5bf46b4ff270b077bc23a0c6688d6127d17285
El Ouakili, Abdelghani (IntlAssoc) <abdelghani.elouakili@nist.net>
2017-03-31 12:24:56 -0400

    fix before demo

--- igamt-lite-client/app/scripts/controllers/main.js
+++ igamt-lite-client/app/scripts/controllers/main.js
@@ -1232,12 +1232,7 @@ angular.module('igl').controller('MainCtrl', ['$document', '$scope', '$rootScope
                                         $rootScope.tables.push(table);
                                         $rootScope.tablesMap[table.id]=table;
                                         console.log($rootScope.datatypesMap[$scope.DatatypeToAdd.id]);
-
-
                                     }
-
-
-
                                 });
                                 $rootScope.confirmSwitch($rootScope.datatype,$rootScope.datatypesMap[$scope.DatatypeToAdd.id]);
 
@@ -3749,6 +3744,23 @@ angular.module('igl').controller('MainCtrl', ['$document', '$scope', '$rootScope
 
         });
     };
+    $scope.getDatatypeForUpgrade= function(id){
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
 
     $rootScope.getGroupNodeName = function(node) {
         return node.position + "." + node.name;
