0b42867603dde9198aa3254d3dc90d34b311b589
El Ouakili, Abdelghani (IntlAssoc) <abdelghani.elouakili@nist.net>
2017-08-11 16:40:59 -0400

    display filter scrollable content for tree table

--- igamt-lite-client/app/scripts/directives/label.js
+++ igamt-lite-client/app/scripts/directives/label.js
@@ -21,3 +21,14 @@ angular.module('igl')
             } //DOM manipulation
         }
     });
+angular.module('igl').directive('noDirtyCheck', function() {
+    // Interacting with input elements having this directive won't cause the
+    // form to be marked dirty.
+    return {
+        restrict: 'A',
+        require: 'ngModel',
+        link: function(scope, elm, attrs, ctrl) {
+            ctrl.$setDirty = angular.noop;
+        }
+    };
+});
