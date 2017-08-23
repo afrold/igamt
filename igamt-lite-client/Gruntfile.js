18adce4b34b42e6720dfafab933d00dd5aa9da3e
Affo, Harold <haffo@nist.gov>
2017-08-23 17:47:31 -0400

    add readme

--- igamt-lite-client/Gruntfile.js
+++ igamt-lite-client/Gruntfile.js
@@ -508,7 +508,7 @@ module.exports = function (grunt) {
     'useminPrepare',
     'concurrent:dist',
     'postcss',
-     'ngtemplates',
+     // 'ngtemplates',
     'concat',
     'ngAnnotate',
     'copy:dist',
