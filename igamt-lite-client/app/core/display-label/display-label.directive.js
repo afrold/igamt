f6246a89039ef2ef0971db3afe9459a6db0c0551
Affo, Harold <affoharold@yahoo.fr>
2017-09-14 12:02:21 -0400

    finish folder by feature structure

--- igamt-lite-client/app/core/display-label/display-label.directive.js
+++ igamt-lite-client/app/core/display-label/display-label.directive.js
@@ -21,14 +21,3 @@ angular.module('igl')
             } //DOM manipulation
         }
     });
-angular.module('igl').directive('noDirtyCheck', function() {
-    // Interacting with input elements having this directive won't cause the
-    // form to be marked dirty.
-    return {
-        restrict: 'A',
-        require: 'ngModel',
-        link: function(scope, elm, attrs, ctrl) {
-            ctrl.$setDirty = angular.noop;
-        }
-    };
-});
