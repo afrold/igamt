e0d0c2ac6eb813f07c195286bd97765797189bf7
Affo, Harold <affoharold@yahoo.fr>
2017-10-06 14:11:42 -0400

    fixed

---
+++ igamt-lite-domain/src/main/java/gov/nist/healthcare/tools/hl7/v2/igamt/lite/domain/Library.java
@@ -0,0 +1,19 @@
+package gov.nist.healthcare.tools.hl7.v2.igamt.lite.domain;
+
+public class Library extends TextbasedSectionModel implements java.io.Serializable, Cloneable {
+
+  private static final long serialVersionUID = 1L;
+
+  protected LibraryExportConfig exportConfig = new LibraryExportConfig();
+
+  public LibraryExportConfig getExportConfig() {
+    return exportConfig;
+  }
+
+  public void setExportConfig(LibraryExportConfig exportConfig) {
+    this.exportConfig = exportConfig;
+  }
+
+
+
+}
