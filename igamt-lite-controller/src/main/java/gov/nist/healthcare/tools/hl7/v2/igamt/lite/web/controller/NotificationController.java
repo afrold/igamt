91a6335c1758ae5d663f413ab0e737f54d861df9
Woo, Jungyub (Assoc) <jungyub.woo@nist.gov>
2017-11-08 13:34:27 -0500

    working copy

---
+++ igamt-lite-controller/src/main/java/gov/nist/healthcare/tools/hl7/v2/igamt/lite/web/controller/NotificationController.java
@@ -0,0 +1,36 @@
+package gov.nist.healthcare.tools.hl7.v2.igamt.lite.web.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import gov.nist.healthcare.tools.hl7.v2.igamt.lite.domain.Notification;
+import gov.nist.healthcare.tools.hl7.v2.igamt.lite.repo.NotificationRepository;
+import gov.nist.healthcare.tools.hl7.v2.igamt.lite.web.exception.DataNotFoundException;
+
+@RestController
+@RequestMapping("/notifications")
+public class NotificationController extends CommonController {
+  Logger log = LoggerFactory.getLogger(NotificationController.class);
+  @Autowired
+  NotificationRepository notificationRepository;
+  
+  @RequestMapping(value = "/igdocument/{id}", method = RequestMethod.GET, produces = "application/json")
+  public List<Notification> findNotificationByIgId(@PathVariable("id") String id) throws DataNotFoundException {
+    log.info("Fetching findNotificationByIgId..." + id);
+    List<Notification> result= new ArrayList<Notification>();
+    List<Notification> allNotifications=notificationRepository.findAll();
+    
+    for(Notification n:allNotifications){
+      if(n.containIgId(id)) result.add(n);
+    }
+    return result;
+  }
+}
---
+++ igamt-lite-domain/src/main/java/gov/nist/healthcare/tools/hl7/v2/igamt/lite/domain/Notification.java
@@ -0,0 +1,83 @@
+package gov.nist.healthcare.tools.hl7.v2.igamt.lite.domain;
+
+import java.util.Date;
+import java.util.HashSet;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "notification")
+public class Notification {
+  @Id
+  private String id;
+  
+  private TargetType targetType;
+  
+  private String targetId;
+  
+  private Date changedDate;
+  
+  private String byWhom;
+  
+  HashSet<String> igDocumentIds=new HashSet<String>();
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public TargetType getTargetType() {
+    return targetType;
+  }
+
+  public void setTargetType(TargetType targetType) {
+    this.targetType = targetType;
+  }
+
+  public String getTargetId() {
+    return targetId;
+  }
+
+  public void setTargetId(String targetId) {
+    this.targetId = targetId;
+  }
+
+  public Date getChangedDate() {
+    return changedDate;
+  }
+
+  public void setChangedDate(Date changedDate) {
+    this.changedDate = changedDate;
+  }
+
+  public String getByWhom() {
+    return byWhom;
+  }
+
+  public void setByWhom(String byWhom) {
+    this.byWhom = byWhom;
+  }
+
+  public HashSet<String> getIgDocumentIds() {
+    return igDocumentIds;
+  }
+
+  public void setIgDocumentIds(HashSet<String> igDocumentIds) {
+    this.igDocumentIds = igDocumentIds;
+  }  
+  
+  public void addIgId(String id){
+    this.igDocumentIds.add(id);
+  }
+  
+  public void removeIgId(String id){
+    this.igDocumentIds.remove(id);
+  }
+  
+  public boolean containIgId(String id){
+    return this.igDocumentIds.contains(id);
+  }
+}
