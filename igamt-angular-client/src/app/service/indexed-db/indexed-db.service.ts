94817ba5baaa3b4a6826cb1f469e03a1ce0f70cf
Lefort, Maxence C <maxence.lefort@nist.gov>
2017-12-06 12:50:06 -0500

    Update service to store collections of objects

--- igamt-angular-client/src/app/service/indexed-db/indexed-db.service.ts
+++ igamt-angular-client/src/app/service/indexed-db/indexed-db.service.ts
@@ -2,20 +2,55 @@ import { Injectable } from '@angular/core';
 
 import Dexie from 'dexie';
 
-interface ILink {
+export interface IObject {
   id?: string;
-  label?: string;
+  object?: object;
 }
 
-class LinkDatabase extends Dexie {
-  datatypeLink: Dexie.Table<ILink, number>;
-  tableLink: Dexie.Table<ILink, number>;
+/*
+export interface IDatatype {
+  id?: string;
+  datatype?: object;
+}
+export interface IValueSet {
+  id?: string;
+  valueSet?: object;
+}
+export interface ISegment {
+  id?: string;
+  segment?: object;
+}
+export interface ISection {
+  id?: string;
+  section?: object;
+}
+export interface IProfileComponent {
+  id?: string;
+  profileComponent?: object;
+}
+export interface IProfile {
+  id?: string;
+  profile?: object;
+}
+*/
 
-  constructor() {
-    super('LinkDatabase');
+class ObjectsDatabase extends Dexie {
+  datatypes: Dexie.Table<IObject, number>;
+  valueSets: Dexie.Table<IObject, number>;
+  segments: Dexie.Table<IObject, number>;
+  sections: Dexie.Table<IObject, number>;
+  profileComponents: Dexie.Table<IObject, number>;
+  profiles: Dexie.Table<IObject, number>;
+
+  constructor(name) {
+    super(name);
     this.version(1).stores({
-      datatypeLink: '++id,label',
-      tableLink: '++id,label'
+      datatypes: '++id,object',
+      segments: '++id,object',
+      sections: '++id,object',
+      profileComponents: '++id,object',
+      profiles: '++id,object',
+      valueSets: '++id,object'
     });
   }
 }
@@ -23,31 +58,32 @@ class LinkDatabase extends Dexie {
 @Injectable()
 export class IndexedDbService {
 
-  linkDatabase;
+  objectsDatabase;
+  changedObjectsDatabase;
   constructor() {
-    this.linkDatabase = new LinkDatabase();
-    this.linkDatabase.transaction('rw', this.linkDatabase.datatypeLink, async() => {
-      await this.linkDatabase.datatypeLink.add({'id': '1', 'label': 'HD'});
-      await this.linkDatabase.datatypeLink.add({'id': '2', 'label': 'ST'});
+    this.objectsDatabase = new ObjectsDatabase('ObjectsDatabase');
+    this.changedObjectsDatabase = new ObjectsDatabase('ChangedObjectsDatabase');
+    this.objectsDatabase.transaction('rw', this.objectsDatabase.datatypes, async() => {
+      await this.objectsDatabase.datatypes.add({'id': '1', 'object': {'label': 'HD_IZ', 'version': '2.1.5'}});
+      await this.objectsDatabase.datatypes.add({'id': '2', 'object': {'label': 'ST', 'version': '2.1.5'}});
     });
-    this.linkDatabase.transaction('rw', this.linkDatabase.tableLink, async() => {
-      await this.linkDatabase.tableLink.add({'id': '1', 'label': '0001'});
-      await this.linkDatabase.tableLink.add({'id': '2', 'label': '0304'});
+    this.changedObjectsDatabase.transaction('rw', this.changedObjectsDatabase.datatypes, async() => {
+      await this.changedObjectsDatabase.datatypes.add({'id': '1', 'object': {'label': 'HD_IZ_CHANGED', 'version': '2.1.5'}});
     });
+
   }
 
-  public getDatatypeLinks (callback) {
-    this.linkDatabase.transaction('rw', this.linkDatabase.datatypeLink, async() => {
-      const datatypeLinks = await this.linkDatabase.datatypeLink.toArray();
-      callback(datatypeLinks);
+  public getDatatype (id) {
+    const datatype = this.changedObjectsDatabase.transaction('r', this.changedObjectsDatabase.datatypes, async() => {
+      return await this.changedObjectsDatabase.datatypes.get(id);
     });
-  }
-
-  public getDatatypeLink (id, callback) {
-    this.linkDatabase.transaction('rw', this.linkDatabase.datatypeLink, async() => {
-      const datatypeLink = await this.linkDatabase.datatypeLink.get(id);
-      callback(datatypeLink);
-    });
+    if (datatype != null) {
+      return datatype;
+    } else {
+      return this.objectsDatabase.transaction('r', this.objectsDatabase.datatypes, async() => {
+        return await this.objectsDatabase.datatypes.get(id);
+      });
+    }
   }
 
 }
