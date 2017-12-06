e6a11c214f44e41be0174237e636e7181d1d37b3
Lefort, Maxence C <maxence.lefort@nist.gov>
2017-12-06 16:40:57 -0500

    Working version for datatypes

--- igamt-angular-client/src/app/service/indexed-db/indexed-db.service.ts
+++ igamt-angular-client/src/app/service/indexed-db/indexed-db.service.ts
@@ -11,12 +11,14 @@ export class IndexedDbService {
     this.objectsDatabase = new ObjectsDatabase('ObjectsDatabase');
     this.changedObjectsDatabase = new ObjectsDatabase('ChangedObjectsDatabase');
     this.objectsDatabase.transaction('rw', this.objectsDatabase.datatypes, async() => {
-      await this.objectsDatabase.datatypes.add({'id': '1', 'object': {'label': 'HD_IZ', 'version': '2.1.5'}});
-      await this.objectsDatabase.datatypes.add({'id': '2', 'object': {'label': 'ST', 'version': '2.1.5'}});
+      this.objectsDatabase.datatypes.clear();
+      await this.objectsDatabase.datatypes.add({'id': '1', 'object': {'id': '1234', 'label': 'HD_IZ', 'version': '2.1.5'}});
+      await this.objectsDatabase.datatypes.add({'id': '2', 'object': {'id': '5678', 'label': 'ST', 'version': '2.1.5'}});
     });
     this.changedObjectsDatabase.transaction('rw', this.changedObjectsDatabase.datatypes, async() => {
-      await this.changedObjectsDatabase.datatypes.add({'id': '1', 'object': {'label': 'HD_IZ_CHANGED', 'version': '2.1.5'}});
-      await this.changedObjectsDatabase.datatypes.add({'id': '3', 'object': {'label': 'CWE_CHANGED', 'version': '2.1.5'}});
+      this.changedObjectsDatabase.datatypes.clear();
+      await this.changedObjectsDatabase.datatypes.add({'id': '1', 'object': {'id': '1234', 'label': 'HD_IZ_CHANGED', 'version': '2.1.5'}});
+      await this.changedObjectsDatabase.datatypes.add({'id': '3', 'object': {'id': '9101', 'label': 'CWE_CHANGED', 'version': '2.1.5'}});
     });
   }
 
@@ -24,19 +26,14 @@ export class IndexedDbService {
     let datatype;
     this.changedObjectsDatabase.transaction('r', this.changedObjectsDatabase.datatypes, async() => {
       datatype = await this.changedObjectsDatabase.datatypes.get(id);
-    });
-    if (datatype != null) {
-      console.log('datatype value from changedDb' + JSON.stringify(datatype));
-      callback(datatype);
-    } else {
-      this.objectsDatabase.transaction('r', this.objectsDatabase.datatypes, async() => {
-        datatype = await this.objectsDatabase.datatypes.get(id);
+      if (datatype != null) {
         callback(datatype);
-      });
-    }
-    this.objectsDatabase.transaction('r', this.objectsDatabase.datatypes, async() => {
-      const datatype = await this.objectsDatabase.datatypes.get(id);
-      callback(datatype);
+      } else {
+        this.objectsDatabase.transaction('r', this.objectsDatabase.datatypes, async() => {
+          datatype = await this.objectsDatabase.datatypes.get(id);
+          callback(datatype);
+        });
+      }
     });
   }
 
